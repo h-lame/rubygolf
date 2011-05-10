d3840673a3a48a5fc4583f439bc5855d389dd3c5
Murray Steele <muz@h-lame.com>
2011-05-10 22:18:43 +0100

    745: If we can't shorten our holes, lets shorten how we define them. eval-ing strings FTW!
    Some credit to @threedaymonk for his whitespace solution giving me the idea to think sideways.

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,64 +1,49 @@
-class << Golf = ''
-  Z=-1
-  def hole1 n
+Golf = ''
+Z=-1
+B=[0,
+  '
     n.inject :*
-  end
-
-  def hole2 n
-    n.split.sort_by{|w| w[1]} * ' '
-  end
-
-  def hole3 n
+  ','
+    n.split.sort_by{|w| w[1]} * " "
+  ','
     hole1(1..n)
-  end
-
-  def hole4 n
-    n.map { |w| 
+  ',%q{
+    n.map { |w|
       w =~ /man/ ? "hat(#{w})" : w.gsub(/(dog.*)\)$/, '\1(bone))').gsub(/cat/, 'dead')
     }
-  end
-
-  def hole5 n
+  },'
     n == [] ? [] : (
       h, *t = n
       o = [[h]]
       t.size.times { |x|
         o << [h, *t[0..x]]
       }
-      (o + hole5(t)).sort {|a,b| 
+      (o + hole5(t)).sort {|a,b|
         x = a.size <=> b.size
         x == 0 ? a[0] <=> b[0] : x
       }
     )
-  end
-
-  def hole6 n
+  ','
     b = "buzz"
     (1..n).map { |x|
       r = x % 3 == 0 ? "fizz" : x
       x % 5 == 0 ? r.to_s == r ? r += b : r = b : r
     }
-  end
-
-  def hole7 n
+  ','
     n[1..Z].inject([[n[0]]]) { |r, x|
       r[Z][Z]+1 == x ? r[Z] << x : r << [x]
       r
-    }.map { |x| [x[0], x[Z]].uniq * '-' }
-  end
-
-  def hole8 n
+    }.map { |x| [x[0], x[Z]].uniq * "-" }
+  ','
     x = [1] * n
     2.upto(n-1) { |i| x[i] = x[i-1] + x[i-2]}
     x
-  end
-
-  def hole9 n
+  ','
     l = IO.readlines n
     t = l.size / 2
-    d = l.map{|r| r.split(',').map &:strip }
+    d = l.map{|r| r.split(",").map &:strip }
     w = proc { |d|
-      v = d.map { |v| v[0] }.inject(Hash.new 0) { |h,c| 
+      v = d.map { |v| v[0] }.inject(Hash.new 0) { |h,c|
         h[c] = h[c] + 1
         h
       }
@@ -66,5 +51,10 @@ class << Golf = ''
       r[0][Z] >= t ? r[0][0] : w[d.map { |v| v[0] == r[Z][0] ? v[1..Z] : v }]
     }
     w[d]
-  end
-end
+  ']
+(1..9).map {|i|
+  eval "
+def Golf.hole#{i} n
+  #{B[i]}
+end"
+}
